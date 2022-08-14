38b2e0d09c076fab443b5d34a7dc058c39d5de2e
pty819 <pty819@outlook.com>
2022-08-14 23:59:10 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6
 ./dotnet6/dotnet --version
 ./dotnet6/dotnet publish -c Release -o output -p:BlazorEnableCompression=true
+rm output/wwwroot/_framework/*.wasm
