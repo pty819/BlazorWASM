5e2c19d15370130e88c85918835f9c8656a60337
pty819 <pty819@outlook.com>
2022-12-30 10:30:51 +0800

    update to dotnet 7

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6
-./dotnet6/dotnet --version
-./dotnet6/dotnet publish -c Release -o output -p:BlazorEnableCompression=true
+./dotnet-install.sh -c 7.0 -InstallDir ./dotnet
+./dotnet/dotnet --version
+./dotnet/dotnet publish -c Release -o output -p:BlazorEnableCompression=true
 rm output/wwwroot/_framework/*.wasm
