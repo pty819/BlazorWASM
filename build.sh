3d26b47dc3b64a52a3ac17fa8953cf0d4ad678a8
pty819 <pty819@outlook.com>
2022-12-30 10:41:48 +0800

    wasm-tools

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 7.0 -InstallDir ./dotnet
-./dotnet workload install wasm-tools
+./dotnet/dotnet workload install wasm-tools
 ./dotnet/dotnet --version
 ./dotnet/dotnet publish -c Release -o output -p:BlazorEnableCompression=true
 rm output/wwwroot/_framework/*.wasm
