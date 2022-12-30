9b339901f0fa37ce54a28bd5181d9052a3732c45
pty819 <pty819@outlook.com>
2022-12-30 10:50:03 +0800

    remove compress

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 7.0 -InstallDir ./dotnet
-./dotnet/dotnet workload install wasm-tools
+#./dotnet/dotnet workload install wasm-tools
 ./dotnet/dotnet --version
-./dotnet/dotnet publish -c Release -o output -p:BlazorEnableCompression=true
-
-#rm output/wwwroot/_framework/*.wasm
+./dotnet/dotnet publish -c Release -o output
+#./dotnet/dotnet publish -c Release -o output -p:BlazorEnableCompression=true
+rm output/wwwroot/_framework/*.wasm
